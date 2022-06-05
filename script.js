565102d7db6f45e60f17711132e02f71ecbb7c34
GuilhermeHalter <guisam.halter@gmail.com>
2022-06-05 17:22:37 -0300

    ultimas alterações

---
+++ script.js
@@ -0,0 +1,9 @@
+var nota1 = parseInt(prompt("Digite nota 1: "));
+var nota2 = parseInt(prompt("Digite nota 2: "));
+var nota3 = parseInt(prompt("Digite nota 3: "));
+var media = (nota1+nota2+nota3)/3;
+
+document.write("Sua primeira nota foi ", nota1, "<br/>")
+document.write("Sua segunda nota foi ", nota2, "<br/>")
+document.write("Sua terceira nota foi ", nota3, "<br/>")
+document.write("Sua média foi de ", media);
